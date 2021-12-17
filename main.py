5c782182849ef66fd13c24a63f7c90ab2fa178b8
meganlukes1 <82395818+meganlukes@users.noreply.github.com>
2021-12-17 19:10:26 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+programming_dictionary = {
+  "Bug": "An error in a program that prevents the program from running as expected.", 
+  "Function": "A piece of code that you can easily call over and over again." 
+  }
+
+#retrieving items from dictionary
+print(programming_dictionary["Bug"])
+ 
+#adding new items to dictionary
+programming_dictionary["Loop"] = "The action of doing something over and over."
+print(programming_dictionary)
+
