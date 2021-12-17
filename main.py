e7443b030dc27be68b2fa2f26e92c2dac7695517
meganlukes1 <82395818+meganlukes@users.noreply.github.com>
2021-12-17 19:16:07 +0000

    how to manipulate lists with keys

--- main.py
+++ main.py
@@ -4,9 +4,22 @@ programming_dictionary = {
   }
 
 #retrieving items from dictionary
-print(programming_dictionary["Bug"])
+# print(programming_dictionary["Bug"])
  
 #adding new items to dictionary
 programming_dictionary["Loop"] = "The action of doing something over and over."
 print(programming_dictionary)
+print("")
+# #to wipe an existing dictionary
+# programming_dictionary = {}
 
+# #to edit an item in dictionary
+# programming_dictionary["Bug"] = "An insect"
+# print(programming_dictionary)
+
+
+for thing in programming_dictionary:
+  #to get a list of library's keys
+  print(thing)
+  #to get a list of values
+  print(programming_dictionary[thing])
